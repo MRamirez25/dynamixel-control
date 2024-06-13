3f83b22726e1ab62c06efcc907f5e066eedafaf7
Mariano Ramirez <M.RamirezMontero-1@student.tudelft.nl>
2024-06-13 16:33:56 +0200

    Added ros package things

---
+++ dynamixel_control/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dynamixel_control',
+    version='0.0.0',
+    description='',
+    author='Mariano Ramirez',
+    author_email='',
+    packages=find_packages(),
+    # Add other dependencies here
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
