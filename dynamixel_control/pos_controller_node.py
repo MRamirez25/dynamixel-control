05eb38004911606455b078d03d4fad145cd52d26
Mariano Ramirez <nano2598.r@gmail.com>
2024-12-13 18:17:54 +0100

    Adds possibility to move single servos apart from moving them all at ance, also can record currents now

--- dynamixel_control/pos_controller_node.py
+++ dynamixel_control/pos_controller_node.py
@@ -11,42 +11,59 @@ from pynput.keyboard import Listener, Key
 
 class PosController:
     def __init__(self) -> None:
-        device_name = rospy.get_param('/dynamixels/device_name', '/dev/ttyUSB0')
+        device_name = rospy.get_param('/dynamixels/device_name', '/dev/my_dynamixel')
         self.increment = rospy.get_param('/dynamixels/control_increment', 50)
-        ids = rospy.get_param('/dynamixels/ids', [1])
+        ids = rospy.get_param('/dynamixels/ids', [1,2,9])
         self.config = Config(device_name=device_name)
         self.robot = Robot(config=self.config, ids=ids)
 
-        self.move_cw_sub = rospy.Subscriber('/move_dynamixels_cw', Bool, self.move_cw_cb)
-        self.move_ccw_sub = rospy.Subscriber('/move_dynamixels_ccw', Bool, self.move_ccw_cb)
+        self.move_cw_sub = rospy.Subscriber('/move_dynamixels_cw', Bool, self.move_all_cw_cb)
+        self.move_ccw_sub = rospy.Subscriber('/move_dynamixels_ccw', Bool, self.move_all_ccw_cb)
+        self.move_id1_sub = rospy.Subscriber('/move_dynamixel_id1', Float32, self.move_id1_cb)
+        self.move_id2_sub = rospy.Subscriber('/move_dynamixel_id2', Float32, self.move_id2_cb)
         # self.listener = Listener(on_press=self._on_press, on_release=self._on_release)
         # self.listener.start()
-        self.move_cw_state = False
-        self.move_ccw_state = False
+        self.move_all_cw_state = False
+        self.move_all_ccw_state = False
+        self.move_id1_state = 0
+        self.move_id2_state = 0
         self.stop = False
 
-    def move_cw_cb(self, msg):
+    def move_all_cw_cb(self, msg):
         # print(self.present_positions)
-        self.move_cw_state  = msg.data
+        self.move_all_cw_state  = msg.data
 
-
-    def move_ccw_cb(self, msg):
+    def move_all_ccw_cb(self, msg):
         # print(self.present_positions)
-        self.move_ccw_state = msg.data
+        self.move_all_ccw_state = msg.data
+    
+    def move_id1_cb(self, msg):
+        self.move_id1_state = msg.data
+    
+    def move_id2_cb(self, msg):
+        self.move_id2_state = msg.data
 
-    def move_cw(self):
+    def move_all(self, direction):
+        self.present_positions = self.robot.get_positions_sync()
+        self.present_currents = self.robot.get_currents_sync()
         position_commands = {}
         for id in self.robot.ids:
-            position_commands[id] = int(self.present_positions[id] + self.increment)
+            position_commands[id] = int(self.present_positions[id] + direction*self.increment)
         self.robot.move_pos_sync(position_commands, relative_to_init=False)
-        self.present_positions = self.robot.get_positions_sync()
 
-    def move_ccw(self):
-        position_commands = {}
-        for id in self.robot.ids:
-            position_commands[id] = int(self.present_positions[id] - self.increment)
-        self.robot.move_pos_sync(position_commands, relative_to_init=False)
+    def move_single(self, id, direction):
         self.present_positions = self.robot.get_positions_sync()
+        self.present_currents = self.robot.get_currents_sync()
+        position_commands = copy.deepcopy(self.present_positions)
+        position_commands[id] = int(self.present_positions[id] + direction*self.increment)
+        self.robot.move_pos_sync(position_commands, relative_to_init=False)
+
+    # def move_all_ccw(self):
+    #     self.present_positions = self.robot.get_positions_sync()
+    #     position_commands = {}
+    #     for id in self.robot.ids:
+    #         position_commands[id] = int(self.present_positions[id] - self.increment)
+    #     self.robot.move_pos_sync(position_commands, relative_to_init=False)
 
     def run(self):
         rospy.spin()
@@ -59,9 +76,9 @@ if __name__ == '__main__':
     pos_controller.present_positions = {id: pos_controller.robot.initial_positions[id] for id in pos_controller.robot.ids}
 
     while not rospy.is_shutdown():
-        if pos_controller.move_cw_state:
-            pos_controller.move_cw()
-        elif pos_controller.move_ccw_state:
-            pos_controller.move_ccw()
+        if pos_controller.move_all_cw_state:
+            pos_controller.move_all(direction=+1)
+        elif pos_controller.move_all_ccw_state:
+            pos_controller.move_all(direction=-1)
         
 # %%
