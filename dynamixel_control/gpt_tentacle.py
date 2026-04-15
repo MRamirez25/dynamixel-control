2ac983a9d8881c6646959964d11210d31ce243db
Mariano Ramirez <nano2598.r@gmail.com>
2026-04-16 00:32:32 +0200

    Adds most recent version of main experiments script

--- dynamixel_control/gpt_tentacle.py
+++ dynamixel_control/gpt_tentacle.py
@@ -1,14 +1,14 @@
 """
-Authors: Ravi Prakash & Giovanni Franzese, March 2023
-Email: g.franzese@tudelft.nl, r.prakash@tudelft.nl
-Cognitive Robotics, TU Delft
-This code is part of TERI (TEaching Robots Interactively) project
+Code modified from https://github.com/franzesegiovanni/policy_transportation/blob/devel/ros_ws/script/modules.py,
+by Giovanni Franzese, adding the GPT_tag_tentacle class, which is a combination of the GaussianProcessTransportation 
+and Tag_Detector modules, but using the SimpleDynamixels class.
 """
 from policy_transportation import GaussianProcessTransportation as Transport
 from tag_detector import Tag_Detector
 
 from simple_dynamixels import SimpleDynamixels
 import rospy
+from SIMPLe.SIMPLe import SIMPLe
 
 class GPT_tag_tentacle(SimpleDynamixels, Transport, Tag_Detector):
     def __init__(self):
@@ -20,3 +20,8 @@ class GPT_tag_tentacle(SimpleDynamixels, Transport, Tag_Detector):
 #     def __init__(self):
 #         super(GPT_2d, self).__init__()
         
+class GPT_tag(Transport, Tag_Detector, SIMPLe):
+    def __init__(self):
+        rospy.init_node('GPT', anonymous=True)
+        rospy.sleep(2)
+        super(GPT_tag,self).__init__()
